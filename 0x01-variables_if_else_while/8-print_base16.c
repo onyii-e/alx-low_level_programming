14d6aa31bb7b23e3bfa18c385eaa7c49fb49ad68
onyii-e <onyinyechinwaneri00@gmail.com>
2023-09-08 17:02:28 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,12 +7,13 @@
 
 int main(void)
 {
-int i;
-char j;
-for (i = 0 ; i < 10 ; i++)
-	putchar(i + '0');
-for (j = 'a' ; j <= 'f' ; j++)
-	putchar(j);
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
 	putchar('\n');
-return (0);
+	return (0);
 }
