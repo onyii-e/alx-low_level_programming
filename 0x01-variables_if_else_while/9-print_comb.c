a01c4181b5d72a0315d4ce7c2d26ee75b128ae21
onyii-e <onyinyechinwaneri00@gmail.com>
2023-09-08 17:20:03 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,15 @@ int main(void)
 int i;
 for (i = 0 ; i < 10 ; i++)
 {
-putchar(i);
+if (i == 9)
+putchar(i + '0');
+else
+{
+putchar(i + '0');
 putchar(',');
 putchar(' ');
 }
-putchar('\n');
+}
 return (0);
 }
 
