6878a8af16ae29f1abbc3754c57b2fa1198a80e8
onyii-e <onyinyechinwaneri00@gmail.com>
2023-09-13 05:51:59 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,14 +13,14 @@ int main(void)
 	int i;
 	unsigned long int a = 1, b = 2, next;
 
-	Printf("%lu, %lu, ", a, b);
+	printf("%lu, %lu, ", a, b);
 	for (i = 3; i <= 98; i++)
 	{
 		next = a + b;
 		if (i != 98)
-			Printf("%lu, ", next);
+			printf("%lu, ", next);
 		else
-			Printf("%lu\n", next);
+			printf("%lu\n", next);
 		a = b;
 		b = next;
 	}
