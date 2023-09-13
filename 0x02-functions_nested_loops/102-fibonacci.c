83390c22de784b234c111666ead6a2ecb8589552
onyii-e <onyinyechinwaneri00@gmail.com>
2023-09-13 05:08:43 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,9 +2,8 @@
 
 /**
  * main - Print first 50 Fibonacci numbers, starting with 1 and 2,
- *        separated by a comma followed by a space
- * Return:always 0
- *
+ *        separated by a comma followed by a space.
+ * Return:always 0.
  */
 
 int main(void)
@@ -24,7 +23,7 @@ int main(void)
 		if (count == 49)
 			printf("\n");
 		else
-			printf(",");
+			printf(", ");
 	}
 
 	return (0)
