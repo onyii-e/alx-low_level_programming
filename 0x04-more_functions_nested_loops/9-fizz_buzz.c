0d032d94015f4e83eeae2ce38c50021a79781ac8
onyii-e <onyinyechinwaneri00@gmail.com>
2023-09-14 09:50:05 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -22,12 +22,13 @@ int main(void)
 		else if (i % 3 == 0)
 			printf("Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz")
+			printf("Buzz");
 		else
 			printf("%i", i);
 		if (i < 100)
 			printf(" ");
 	}
 	printf("\n");
+
 	return (0);
 }
