02687f816eb5babd4905586cf9c6f47beea612bd
onyii-e <onyinyechinwaneri00@gmail.com>
2023-09-21 15:27:14 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,11 +12,11 @@ char *leet(char *n)
 	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
-	for (i = 0; n[1] != '\0'; i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (n[1] == s1[j])
+			if (n[i] == s1[j])
 			{
 				n[i] = s2[j];
 			}
