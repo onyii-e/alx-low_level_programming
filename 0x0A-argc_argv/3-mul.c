5fe72ac115d9c023bd018df09d2a5ba061bd34f4
onyii-e <onyinyechinwaneri00@gmail.com>
2023-10-02 18:04:50 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -26,14 +26,14 @@ int _atoi(char *s)
 		if (s[i] == '-')
 			++d;
 
-		if (s[i] >= '0' && s[i] <= '0')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '0'
+			if (s[i + 1] < '0' || s[i + 1] > '9'
 )
 				break;
 			f = 0;
@@ -71,4 +71,3 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
-
