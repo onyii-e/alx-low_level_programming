6afb34b6b9a903647f1acf6ea3cc66581e8e8dc6
onyii-e <onyinyechinwaneri00@gmail.com>
2023-10-04 13:38:06 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -38,25 +38,18 @@ int wrdcnt(char *s)
 char **strtow(char *str)
 {
 	int i, j, k, l, n = 0, wc = 0;
-
 	char **w;
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
 	n = wrdcnt(str);
-
 	if (n == 1)
 		return (NULL);
-
 	w = (char **) malloc(n * sizeof(char *));
-
 	if (w == NULL)
 		return (NULL);
-
 	w[n - 1] = NULL;
-
 	i = 0;
-
 	while (str[i])
 	{
 		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
@@ -66,29 +59,22 @@ char **strtow(char *str)
 			j++;
 			w[wc] = (char *)malloc(j * sizeof(char));
 			j--;
-
 			if (w[wc] == NULL)
 			{
 				for (k = 0; k < wc; k++)
 					free(w[k]);
 				free(w[n - 1]);
 				free(w);
-
 				return (NULL);
 			}
-
 			for (l = 0; l < j; l++)
 				w[wc][l] = str[i + l];
 			w[wc][l] = '\0';
-
 			wc++;
-
 			i += j;
 		}
-
 		else
 			i++;
 	}
-
 	return (w);
 }
