6bab81d0e34d07651779145764bf00416d399100
onyii-e <onnyinyechinwaneri00@gmail.com>
2023-12-07 08:37:23 -0800

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * dlistsint_len - returns the number of elements in
+ * dlistint_len - returns the number of elements in
  * a double linked list
  *
  * @h: head of the list
