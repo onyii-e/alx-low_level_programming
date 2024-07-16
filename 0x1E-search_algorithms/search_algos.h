ff1bcf37e8f2485b1cdfce6ad53869dc3001bea5
onyii-e <onyinyechinwaneri00@gmail.com>
2024-07-16 16:48:58 -0700

    skiplist

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,10 +3,33 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer stored in the node
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node in the list
+ * @express: Pointer to the next node in the express lane
+ */
+
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t l, size_t r);
 int binary_search_index(int *array, size_t l, size_t r, int value);
+
+skiplist_t *linear_skip(skiplist_t *list, int value);
+skiplist_t *create_skiplist(int *array, size_t size);
+void print_skiplist(const skiplist_t *list);
+void free_skiplist(skiplist_t *list);
 
 #endif /* _search_algos_ */
